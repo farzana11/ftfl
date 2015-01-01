310152dc1f69c84ad3821dde2336768ca60b6f19
ahmedanjumfahad <f20_777@yahoo.com>
2015-01-01 14:08:32 +0600

    multiple

--- add_in.php
+++ add_in.php
@@ -6,22 +6,31 @@ echo '<pre>' ;
 print_r($_POST);
 echo '</pre>' ;
 
-//$hobby=implode(',',$_POST["hobby"]);
-//echo ($hobby)
+
+
 
 $hobby = '';
 
-if(isset($_POST['hobby_reading']) && !empty($_POST['hobby_reading'])){
+if(array_key_exists('hobby_reading',$_POST)&& !empty($_POST['hobby_reading'])){
     $hobby = $hobby . $_POST['hobby_reading'];
 }
 
-if(isset($_POST['hobby_gardening']) && !empty($_POST['hobby_gardening'])){
+if(array_key_exists('hobby_gardening',$_POST) && !empty($_POST['hobby_gardening'])){
     $hobby = $hobby .','. $_POST['hobby_gardening'];
 }
-if(isset($_POST['hobby_travelling']) && !empty($_POST['hobby_travelling'])) {
+if(array_key_exists('hobby_travelling',$_POST) && !empty($_POST['hobby_travelling'])) {
     $hobby = $hobby .','. $_POST['hobby_travelling'];
 }
-    if(isset($_POST['hobby_fishing']) && !empty($_POST['hobby_fishing'])) {
-        $hobby = $hobby .','. $_POST['hobby_fishing'];
-    }
-        echo $hobby;
+if(array_key_exists('hobby_fishing',$_POST) && !empty($_POST['hobby_fishing'])) {
+    $hobby = $hobby .','. $_POST['hobby_fishing'];
+}
+echo $hobby;
+echo '<br />';
+
+$food=implode(',',$_POST['food']);
+echo $food ;
+
+echo '<br />';
+
+$city=implode(',',$_POST['city']);
+echo $city ;
